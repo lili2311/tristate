fc86bc822ffb9455eb8deff5637f7ca2c860be21
Liliana Kastilio <kastilio.liliana@gmail.com>
2014-06-12 23:08:37 +0100

    first commit

---
+++ css/scroll.js
@@ -0,0 +1,15 @@
+function init() {
+  window.addEventListener('scroll', function(e){
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 200,
+        header = document.querySelector("header");
+    if (distanceY > shrinkOn) {
+      header.setAttribute("class","smaller");
+    } else {
+        header.removeAttribute("class");
+    }
+  });
+}
+var map;
+
+window.onload = init();
